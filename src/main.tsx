9b4529506a38d470394c32088ee57b4d50b9601e
Elias666-ap <eamayaparco@gmail.com>
2026-02-10 22:48:23 -0500

    Fix main.tsx unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Portfolio from './Components/Portfolio.tsx'
+import Portfolio from './Components/Portfolio'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>,
+  <Portfolio />
 )
